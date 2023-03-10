6c250834c2d1c7c575386b9f9dcc13bfb375cd10
unknown <almajulio04@gmail.com>
2023-03-11 00:32:59 +0700

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Components/Header";
 import Leaderboard from "./Components/Leaderboard";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import Home from "./Pages/Home";
 import Quiz from "./Pages/Quiz";
 import Stats from "./Pages/Stats";
@@ -11,14 +11,14 @@ function App() {
 	return (
 		<div className="App">
 			<Header />
-			<BrowserRouter>
+			<HashRouter>
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/leaderboard" element={<Leaderboard />} />
 					<Route path="/quizes" element={<Quiz />} />
 					<Route path="/stats" element={<Stats />} />
 				</Routes>
-			</BrowserRouter>
+			</HashRouter>
 		</div>
 	);
 }

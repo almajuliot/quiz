9d2346376c24f34bf4842b3160ad0ed1da1237a0
unknown <almajulio04@gmail.com>
2023-03-12 19:07:53 +0700

    tes

--- src/App.js
+++ src/App.js
@@ -7,8 +7,22 @@ import Home from "./Pages/Home";
 import Quiz from "./Pages/Quiz";
 import Stats from "./Pages/Stats";
 import Leaderboards from "./Pages/Leaderboards";
+import { useEffect } from "react";
 
 function App() {
+	const [leaderData, setLeaderData] = [];
+
+	useEffect(() => {
+		async function fethedData() {
+			const res = await fetch("http://5.75.180.91:3000/api/leaderboards");
+			const data = await res.json();
+			setLeaderData(data);
+		}
+		fethedData();
+	}, []);
+
+	console.log(leaderData);
+
 	return (
 		<div className="App">
 			{/* <Header /> */}
